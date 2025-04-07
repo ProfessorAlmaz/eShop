992b5030fa49883c6c5fbc78fa505ffd2aecb471
ProfessorAlmaz <almaz-salahov1998@mail.ru>
2025-04-07 22:34:44 +0300

    18.4

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from shop.models import (
+    Product,
+    ProductImage,
+    Attribute
+)
 # Register your models here.
+
+# admin.site.register(Product)
+# admin.site.register(ProductImage)
+# admin.site.register(Attribute)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'stock')
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('image', 'product')
+
+@admin.register(Attribute)
+class AttributeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
