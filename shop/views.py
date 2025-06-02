e5ac892d55de693a20d0e3c099ab3e0bd5099c62
unknown <almaz-salahov1998@mail.ru>
2025-06-02 17:16:11 +0300

    DTL

--- shop/views.py
+++ shop/views.py
@@ -4,13 +4,6 @@ from django.http import HttpResponse, HttpRequest
 from shop.models import Product
 
 def all_products(request: HttpRequest):
-    print(f"{request.method=}")
-    print(f"{request.content_type=}")
-    print(f"{request.GET=}")
-
-    product_titles = Product.objects.values_list("title", flat=True)
-    titles = b""
-    for title in product_titles:
-        titles += title.encode("utf-8") + b"\n"
-        print(f"{titles.decode('utf-8')=}")
-    return HttpResponse(titles)
+    products = Product.objects.all()
+    return render(request, template_name='index.html',
+        context={'products': products})
