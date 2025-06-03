03f272fcb60140a5a23fcf11a30e9df8ece8dca0
unknown <almaz-salahov1998@mail.ru>
2025-06-03 08:50:22 +0300

    DTL2.3

--- shop/views.py
+++ shop/views.py
@@ -6,5 +6,4 @@ from shop.models import Product
 def all_products(request: HttpRequest):
     current_time = datetime.now()
     products = Product.objects.all()
-    return render(request, template_name='products.html',
-        context={'current_time': current_time, 'products': products})
+    return render(request, template_name='products.html', context={'current_time': current_time, 'products': products})
