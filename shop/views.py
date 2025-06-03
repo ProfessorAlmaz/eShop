34098b2feffb7c90a8aa3f7cbedd5b78b81efc84
unknown <almaz-salahov1998@mail.ru>
2025-06-03 08:45:53 +0300

    DTL2.1

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from datetime import datetime
 from django.http import HttpResponse, HttpRequest
 from shop.models import Product
 
 def all_products(request: HttpRequest):
+    current_time = datetime.now()
     products = Product.objects.all()
     return render(request, template_name='products.html',
         context={'products': products})
