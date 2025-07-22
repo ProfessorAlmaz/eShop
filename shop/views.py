2aa59aeaa487bc6554345d3a30da639cef8dff2c
unknown <almaz-salahov1998@mail.ru>
2025-07-22 17:39:49 +0300

    register2

--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from datetime import datetime
 from django.http import HttpResponse, HttpRequest
 from shop.models import Product
-from django.contrib.auth.forms import UserCreationForm
+from shop.forms import CustomUserCreationForm
 
 def all_products(request: HttpRequest):
     current_time = datetime.now()
     products = Product.objects.all()
-    return render(request, template_name='products.html', context={'current_time': current_time, 'products': products})
+    return render(request, template_name='products.html', context={
+        'current_time': current_time,
+        'products': products,
+        "is_authenticated": request.user.is_authenticated})
 
 def register_page(request: HttpRequest):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("all_products")
+    form = CustomUserCreationForm()
     return render(request, "registration.html", context={"form": form})
