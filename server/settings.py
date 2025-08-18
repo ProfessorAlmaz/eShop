75f94a9efd6015c9651567821105bc69b0c497fb
unknown <almaz-salahov1998@mail.ru>
2025-08-18 19:49:59 +0300

    Deploy2

--- server/settings.py
+++ server/settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'shop.middleware.TMiddleware',
+    #'shop.middleware.TMiddleware',
     'shop.middleware.DiscountMiddleware',
 ]
 
