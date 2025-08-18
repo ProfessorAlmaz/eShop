a32333ec7f3ba56fb7d7999bd458d40a1ef0a725
unknown <almaz-salahov1998@mail.ru>
2025-08-18 19:52:20 +0300

    Deploy2

--- server/settings.py
+++ server/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '45.89.27.48', '127.0.0.1']
 
 
 # Application definition
